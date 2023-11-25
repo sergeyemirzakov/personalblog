7bb95e3e2de3b34995eaac918cdf58441a35b240
Sergey Emirzakov <im.seem16@gmail.com>
2023-11-26 00:25:34 +0500

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import Layout from '@/components/Layout';
+
+const ErrorPage = () => {
+  return (
+    <Layout>
+      <p>This page does not exist so far</p>
+    </Layout>
+  );
+};
+
+export default ErrorPage;
